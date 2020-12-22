36bf9379ca758f2e8d3214caa3d6ec18cbd2784f
Eric Zimmerman <saericzimmerman@gmail.com>
2020-12-21 22:39:58 -0500

    luid

--- SrumData/Srum.cs
+++ SrumData/Srum.cs
@@ -66,6 +66,7 @@ namespace SrumData
         public long BytesReceived;
         public long BytesSent;
         public long InterfaceLuid;
+        public InterfaceType InterfaceType;
         public int L2ProfileFlags;
         public int L2ProfileId;
 
@@ -81,12 +82,161 @@ namespace SrumData
             BytesSent = bytesSent;
             InterfaceLuid = interfaceLuid;
             L2ProfileFlags = l2ProfileFlags;
-            l2ProfileId = l2ProfileFlags;
+            L2ProfileId = l2ProfileId;
+            InterfaceType = Srum.GetInterfaceTypeFromLuid(interfaceLuid);
         }
 
     }
     
+      public enum InterfaceType
+        {
+            IF_TYPE_CES=133,
+IF_TYPE_COFFEE=132,
+IF_TYPE_TUNNEL=131,
+IF_TYPE_A12MPPSWITCH=130,
+IF_TYPE_L3_IPXVLAN=137,
+IF_TYPE_L3_IPVLAN=136,
+IF_TYPE_L2_VLAN=135,
+IF_TYPE_ATM_SUBINTERFACE=134,
+IF_TYPE_MEDIAMAILOVERIP=139,
+IF_TYPE_DIGITALPOWERLINE=138,
+IF_TYPE_SOFTWARE_LOOPBACK=24,
+IF_TYPE_EON=25,
+IF_TYPE_ETHERNET_3MBIT=26,
+IF_TYPE_NSIP=27,
+IF_TYPE_BASIC_ISDN=20,
+IF_TYPE_PRIMARY_ISDN=21,
+IF_TYPE_PROP_POINT2POINT_SERIAL=22,
+IF_TYPE_PPP=23,
+IF_TYPE_SLIP=28,
+IF_TYPE_ULTRA=29,
+IF_TYPE_DDN_X25=4,
+IF_TYPE_ISO88024_TOKENBUS=8,
+IF_TYPE_LAP_F=119,
+IF_TYPE_V37=120,
+IF_TYPE_X25_MLP=121,
+IF_TYPE_X25_HUNTGROUP=122,
+IF_TYPE_TRANSPHDLC=123,
+IF_TYPE_INTERLEAVE=124,
+IF_TYPE_FAST=125,
+IF_TYPE_IP=126,
+IF_TYPE_DOCSCABLE_MACLAYER=127,
+IF_TYPE_DOCSCABLE_DOWNSTREAM=128,
+IF_TYPE_DOCSCABLE_UPSTREAM=129,
+IF_TYPE_HDLC=118,
+IF_TYPE_AFLANE_8023=59,
+IF_TYPE_FRAMERELAY_INTERCONNECT=58,
+IF_TYPE_IEEE80212=55,
+IF_TYPE_PROP_MULTIPLEXOR=54,
+IF_TYPE_HIPPIINTERFACE=57,
+IF_TYPE_FIBRECHANNEL=56,
+IF_TYPE_SONET_VT=51,
+IF_TYPE_SONET_PATH=50,
+IF_TYPE_PROP_VIRTUAL=53,
+IF_TYPE_SMDS_ICIP=52,
+IF_TYPE_ISO88025_FIBER=115,
+IF_TYPE_IPOVER_ATM=114,
+IF_TYPE_ARAP=88,
+IF_TYPE_PROP_CNLS=89,
+IF_TYPE_STACKTOSTACK=111,
+IF_TYPE_IPOVER_CLAW=110,
+IF_TYPE_MPC=113,
+IF_TYPE_VIRTUALIPADDRESS=112,
+IF_TYPE_DS0_BUNDLE=82,
+IF_TYPE_BSC=83,
+IF_TYPE_ATM_LOGICAL=80,
+IF_TYPE_DS0=81,
+IF_TYPE_ISO88025R_DTR=86,
+IF_TYPE_EPLRS=87,
+IF_TYPE_ASYNC=84,
+IF_TYPE_CNR=85,
+IF_TYPE_HDH_1822=3,
+IF_TYPE_IS088023_CSMACD=7,
+IF_TYPE_PPPMULTILINKBUNDLE=108,
+IF_TYPE_IPOVER_CDLC=109,
+IF_TYPE_VOICE_FXS=102,
+IF_TYPE_VOICE_ENCAP=103,
+IF_TYPE_VOICE_EM=100,
+IF_TYPE_VOICE_FXO=101,
+IF_TYPE_ATM_FUNI=106,
+IF_TYPE_ATM_IMA=107,
+IF_TYPE_VOICE_OVERIP=104,
+IF_TYPE_ATM_DXI=105,
+IF_TYPE_SONET=39,
+IF_TYPE_MIO_X25=38,
+IF_TYPE_RS232=33,
+IF_TYPE_FRAMERELAY=32,
+IF_TYPE_SIP=31,
+IF_TYPE_DS3=30,
+IF_TYPE_ATM=37,
+IF_TYPE_ARCNET_PLUS=36,
+IF_TYPE_ARCNET=35,
+IF_TYPE_PARA=34,
+IF_TYPE_AFLANE_8025=60,
+IF_TYPE_CCTEMUL=61,
+IF_TYPE_FASTETHER=62,
+IF_TYPE_ISDN=63,
+IF_TYPE_V11=64,
+IF_TYPE_V36=65,
+IF_TYPE_G703_64K=66,
+IF_TYPE_G703_2MB=67,
+IF_TYPE_QLLC=68,
+IF_TYPE_FASTETHER_FX=69,
+IF_TYPE_REGULAR_1822=2,
+IF_TYPE_ETHERNET_CSMACD=6,
+IF_TYPE_MYRINET=99,
+IF_TYPE_ISO88025_CRFPRINT=98,
+IF_TYPE_TERMPAD=91,
+IF_TYPE_HOSTPAD=90,
+IF_TYPE_X213=93,
+IF_TYPE_FRAMERELAY_MPI=92,
+IF_TYPE_RADSL=95,
+IF_TYPE_ADSL=94,
+IF_TYPE_VDSL=97,
+IF_TYPE_SDSL=96,
+IF_TYPE_STARLAN=11,
+IF_TYPE_ISO88026_MAN=10,
+IF_TYPE_PROTEON_80MBIT=13,
+IF_TYPE_PROTEON_10MBIT=12,
+IF_TYPE_FDDI=15,
+IF_TYPE_HYPERCHANNEL=14,
+IF_TYPE_SDLC=17,
+IF_TYPE_LAP_B=16,
+IF_TYPE_E1=19,
+IF_TYPE_DS1=18,
+IF_TYPE_GIGABITETHERNET=117,
+IF_TYPE_TDLC=116,
+IF_TYPE_MODEM=48,
+IF_TYPE_AAL5=49,
+IF_TYPE_HSSI=46,
+IF_TYPE_HIPPI=47,
+IF_TYPE_FRAMERELAY_SERVICE=44,
+IF_TYPE_V35=45,
+IF_TYPE_LOCALTALK=42,
+IF_TYPE_SMDS_DXI=43,
+IF_TYPE_X25_PLE=40,
+IF_TYPE_ISO88022_LLC=41,
+IF_TYPE_OTHER=1,
+IF_TYPE_RFC877_X25=5,
+IF_TYPE_ISO88025_TOKENRING=9,
+IF_TYPE_IEEE1394=144,
+IF_TYPE_RECEIVE_ONLY=145,
+IF_TYPE_IPFORWARD=142,
+IF_TYPE_MSDSL=143,
+IF_TYPE_DTM=140,
+IF_TYPE_DCN=141,
+IF_TYPE_LAP_D=77,
+IF_TYPE_ISDN_U=76,
+IF_TYPE_ISDN_S=75,
+IF_TYPE_DLSW=74,
+IF_TYPE_ESCON=73,
+IF_TYPE_IBM370PARCHAN=72,
+IF_TYPE_IEEE80211=71,
+IF_TYPE_CHANNEL=70,
+IF_TYPE_RSRB=79,
+IF_TYPE_IPSWITCH=78,
 
+        }
     
     //https://docs.microsoft.com/en-us/windows/win32/extensible-storage-engine/jet-coltyp
     
@@ -119,7 +269,7 @@ namespace SrumData
 
                 var app = IdMap[appId.Value];
                 var user = IdMap[userId.Value];
-
+                
                 var nu = new NetworkUsage(id.Value,dt.Value,app,user,br.Value,bs.Value,iL.Value,pf.Value,pId.Value);
                 
                 NetworkUsages.Add(nu.Id, nu);
@@ -207,9 +357,7 @@ namespace SrumData
 , Api.GetTableColumnid(session, appResourceUsage
 , "L2ProfileId"));
 
-
-                var outVal = string.Empty;
-
+                
 
                 var app = IdMap[appId.Value];
                 var user = IdMap[userId.Value];
@@ -222,6 +370,18 @@ namespace SrumData
             Api.JetResetTableSequential(session, appResourceUsage
 
                 , ResetTableSequentialGrbit.None);
+        }
+
+
+      
+        
+        public static InterfaceType GetInterfaceTypeFromLuid(long val)
+        {
+            var b = BitConverter.GetBytes(val);
+            var intType = BitConverter.ToInt16(b, 6);
+
+            return (InterfaceType) intType;
+
         }
         
         
