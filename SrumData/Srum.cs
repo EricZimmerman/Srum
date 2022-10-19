fc50431fde6249ac0c8616a6fb7d0b5328b632b9
Lior <92099051+LiorSB@users.noreply.github.com>
2022-10-19 09:36:17 +0300

    Fix an exception from "!" in filename
    Hi,
    
    while running the program I ran into an exception.
    The exception is when there is a filename with "!" in it (in my case "osu!.exe"), `tempVal.Split(".exe")` won't work as intended.
    
    What I did is append all of the strings that were caused from additional "!" in a loop.
    Also I changed that the values `Unknown`, `ExeInfoDescription` and `Timestamp` will take their values starting from the last value.
    
    Hope this helps 🙂

--- SrumData/Srum.cs
+++ SrumData/Srum.cs
@@ -28,12 +28,32 @@ public class AppInfo
         var tempVal = ExeInfo.Substring(2); //strip !!
         var segs = tempVal.Split('!');
 
+        if (segs.Length < 4)
+        {
+            return;
+        }
+
         ExeInfo = segs[0];
+        int segsLength = segs.Length;
 
-        Unknown = segs[2];
-        ExeInfoDescription = segs[3];
+        // Incase the filename had "!" in it, append everything back up untill the file type extension.
+        for (int i = 1; i < segsLength - 3; i++)
+        {
+            // If the string is empty it means there were several "!", else append "!<originalValue>"
+            if (segs[i].IsNullOrEmpty())
+            {
+                ExeInfo += "!";
+            }
+            else 
+            {
+                ExeInfo += "!" + segs[i];
+            }
+        }
 
-        Timestamp = DateTimeOffset.ParseExact(segs[1], "yyyy/MM/dd:HH:mm:ss", null,
+        Unknown = segs[segsLength - 2];
+        ExeInfoDescription = segs[segsLength - 1];
+
+        Timestamp = DateTimeOffset.ParseExact(segs[segsLength - 3], "yyyy/MM/dd:HH:mm:ss", null,
             DateTimeStyles.AssumeUniversal);
     }
 
